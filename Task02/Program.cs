63c17dfa78d8dd33c406f71c0ac3b7a18ce2012f
Tatiana Kalinichenko <tatiankalinichenko@gmail.com>
2023-09-09 14:52:26 +0300

    finished Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,3 +3,16 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.WriteLine("Введите первое целое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"{a} больше {b}");
+}
+else
+{
+    Console.WriteLine($"{b} больше {a}");
+}
